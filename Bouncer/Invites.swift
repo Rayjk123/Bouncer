32c1c42a4ed6ab4cb5d7f33cf17534bea154b68e
Rayjk123 <Branden@brandens-macbook-air.local>
2017-03-31 23:40:24 -0400

    update1

---
+++ Bouncer/Invites.swift
@@ -0,0 +1,40 @@
+//
+//  DrawClass.swift
+//  Pods
+//
+//  Created by Branden Lee on 1/23/17.
+//
+//
+
+import UIKit
+import FirebaseDatabase
+
+struct Invites {
+    let inviteQR: String
+    let key: String
+    let ref: FIRDatabaseReference?
+    
+    init(){
+        inviteQR = ""
+        key = ""
+        ref = nil
+    }
+    
+    
+    init(inviteQR: String, key: String = "") {
+        self.key = key
+        self.inviteQR = inviteQR
+        self.ref = nil
+    }
+    
+    init(snapshot: FIRDataSnapshot) {
+        self.key = snapshot.key
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        self.inviteQR = snapshotValue["inviteQR"] as! String
+        self.ref = snapshot.ref
+    }
+    
+    func toAnyObject() -> Any {
+        return ["inviteQR": inviteQR]
+    }
+}
--- Bouncer/ViewController.swift
+++ Bouncer/ViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class InvitesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let storage = FIRStorage.storage()
+    private lazy var eventRef: FIRDatabaseReference = FIRDatabase.database().reference(withPath: "Event").child("Foods")
+    private var eventRefHandle: FIRDatabaseHandle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.blue
-        let button = UIButton(frame: CGRect(x:0, y:0, width:100, height: 100))
-        button.tintColor = UIColor.red
         
-        self.view.addSubview(button)
-        button.setTitle("Hi", for: .normal)
+        setupTableView()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,7 +27,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setupTableView(){
+        let tableview = UITableView()
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "noteCell")
+        tableview.tableFooterView = UIView()
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableview)
+        
+        tableview.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableview.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableview.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
 
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let label = UILabel(frame: CGRect(x: 20, y: 10, width: UIScreen.main.bounds.width - 60, height: 30))
+        label.centerYAnchor.constraint(equalTo: cell.centerYAnchor)
+        label.text = String(indexPath.row)
+        cell.addSubview(label)
+        return cell;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+    
 }
 
