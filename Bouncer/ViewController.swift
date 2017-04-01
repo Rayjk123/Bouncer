dd71b0722fdf555b71fa30171857ce27932940ce
Rayjk123 <Branden@brandens-macbook-air.local>
2017-03-31 22:21:42 -0400

    update

--- Bouncer/ViewController.swift
+++ Bouncer/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
+        let button = UIButton(frame: CGRect(x:0, y:0, width:100, height: 100))
+        button.tintColor = UIColor.red
+        
+        self.view.addSubview(button)
+        button.setTitle("Hi", for: .normal)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
